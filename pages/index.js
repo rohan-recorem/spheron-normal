44c65ff616ef4e334f08c28b5486825c0e103797
rohan-recorem <rajarshi@redefcorp.in>
2023-06-19 12:12:46 +0530

    added env variable main

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div>Hello this is a spheron normal app, this is main branch</div>;
+  return (
+    <div>{`Hello this is a spheron normal app, this is main branch ${process.env.NEXT_PUBLIC_ENV_VAR}`}</div>
+  );
 }
