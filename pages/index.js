a8733cbd2e1460b76e6ce374ec965c81bb4bd68e
rohan-recorem <rajarshi@redefcorp.in>
2023-06-19 12:11:54 +0530

    added env for qa

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div>Hello this is a spheron normal app, this is qa branch</div>;
+  return (
+    <div>{`Hello this is a spheron normal app, this is qa branch ${process.env.NEXT_PUBLIC_ENV_VAR}`}</div>
+  );
 }
